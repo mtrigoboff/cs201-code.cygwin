f00ebe1ec57af10b970077397b42e148e53fd6c8
Michael Trigoboff <michael.trigoboff@pcc.edu>
2015-11-04 18:52:35 -0800

    cleaned up code: LGTH -> lgth, tabs instead of spaces

--- objectCode/stosb.c
+++ objectCode/stosb.c
@@ -1,22 +1,22 @@
 // demonstrates the use of a string operation (stosb)
 // to store '+' into each element of an array of chars
 
-int main (void)
+int main(void)
 {
-    int     LGTH = 5;
-    char    array[] = {'a', 'b', 'c', 'd', 'e'};
+	char	array[] = {'a', 'b', 'c', 'd', 'e'};
+	int		lgth = sizeof(array) / sizeof(char);
     
     // fill array with new character
-    asm("       cld     						\n"		// increment
-    	"       lea     %[array], %%edi         \n"
-        "       mov     $'+', %%al              \n"
-        "       mov     %[LGTH], %%ecx          \n"
-        "       rep stosb                       \n"
+	asm("	cld								\n"		// set to increment
+		"	lea     	%[array], %%edi		\n"
+		"	mov     	$'+', %%al			\n"
+		"	mov     	%[lgth], %%ecx		\n"
+		"	rep stosb						\n"
 
-        :   [array]     "=m"    (array)         // outputs
-        :   [LGTH]      "m"     (LGTH)          // inputs
-        :   "ecx", "edi"                        // clobbers
-    );
+		:	[array]		"=m"	(array)		// outputs
+		:	[lgth]		"m"		(lgth)		// inputs
+		:	"ecx", "edi"					// clobbers
+	);
 
-    return 0;
+	return 0;
 }

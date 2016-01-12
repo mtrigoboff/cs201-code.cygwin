f4ae9ba413e7b344f6cd6d08888207c1000be91e
Michael Trigoboff <michael.trigoboff@pcc.edu>
2016-01-11 17:24:43 -0800

    added mask variable

--- integers/mask.c
+++ integers/mask.c
@@ -11,6 +11,7 @@ int main (int argc, char **argv)
     short       x = 0x1234;
     short       extracted;
     short       insert = 1;
+	short		mask = 0x0018;
 
     if (argc >= 2)                  // did user provide an argument?
         x = atoi(argv[1]);          // use argument as value of x
@@ -23,13 +24,13 @@ int main (int argc, char **argv)
 
     // extract bit field from larger value
     printf("extracting bits 3-4\n");
-    extracted = x & 0x0018;         // mask out the bits we don't want
+    extracted = x & mask;			// mask out the bits we don't want
     extracted >>= 3;                // shift the bits we want to right end
     printf("extracted %d\n\n", extracted);
 
     // insert bit field into larger value
     printf("inserting %d into bits 3-4\n", insert);
-    x &= ~0x0018;                   // zero bits 3-4, no change to rest
+    x &= ~mask;						// zero bits 3-4, no change to rest
     x |= insert << 3;               // insert new value into bits 3-4
     printf("x = 0x%04hX\n\n", x);
 

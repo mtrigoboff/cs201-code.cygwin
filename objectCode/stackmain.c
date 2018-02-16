5ea90729cc6eed2ec4e8c389bb4be55d31a7a98b
Michael Trigoboff <michael.trigoboff@pcc.edu>
2018-02-15 17:15:12 -0800

    simplified code

--- objectCode/stackmain.c
+++ objectCode/stackmain.c
@@ -1,17 +1,12 @@
 // use with insight to demonstrate
 // how argc and argv are stored on the stack
 
-#include <stdio.h>
+#pragma GCC diagnostic ignored "-Wunused-variable"
 
 int main(int argc, char **argv)
 {
-    char    *arg;
-    int     i;
-    
-    for (i = 0; i < argc; i++) {
-        arg = argv[i];
-        printf("arg[%d] = \"%s\"\n", i, arg);
-        }
+    int     i = argc;
+    char    *c = argv[0];
     
     return 0;
 }

f3c2871b576f155dbcf028f139d17ca5ab422778
Michael Trigoboff <michael.trigoboff@pcc.edu>
2020-02-26 17:47:52 -0800

    better to print . before call to read

--- excCtlFlow/processes/ipc/pipes/ipc4.c
+++ excCtlFlow/processes/ipc/pipes/ipc4.c
@@ -58,13 +58,13 @@ int main()
         for (;;) {
 			int		readRet;
 			
+            printf("%c", '.');
+            fflush(stdout);     // override line buffering so we see single char output
 			readRet = read(comm[0], &cmd, 1);
 			if (readRet == -1 && errno == EAGAIN)
 				sleep(1);
 			else
 				break;
-            printf("%c", '.');
-            fflush(stdout);     // override line buffering so we see single char output
         	}
 
         close(comm[0]);

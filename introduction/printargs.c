031cf714e328892aafc8de03b195ace07a29867e
Michael Trigoboff <michael.trigoboff@pcc.edu>
2021-01-19 09:36:06 -0800

    code now responds to argv[0], demo of things like ls

--- introduction/printargs.c
+++ introduction/printargs.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char** argv)
 {
-    printf("there are %d args\n", argc);
-    for (int i = 0; i < argc; i++)
-        printf("%d: \"%s\"\n", i, argv[i]);
-    
-    return 0;
-}
+	if (strcmp(argv[0], "pa") != 0) {			// argv[0] != "pa"
+		
+		// detailed output
+
+		printf("there are %d args\n", argc);
+		for (int i = 0; i < argc; i++)
+			printf("%d: \"%s\"\n", i, argv[i]);
+		}
+
+	else {
+		
+		// short output
+		
+		bool	first = true;
+
+		for (int i = 0; i < argc; i++) {
+			if (first)
+				first = false;
+			else
+				printf(", ");
+			printf("%s", argv[i]);
+			}
+		printf("\n");
+		}
+		
+	return 0;
+	}

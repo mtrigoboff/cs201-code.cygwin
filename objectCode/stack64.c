8d7cb1ef88df888f8841af81a74aec2ea631bf67
Michael Trigoboff <michael.trigoboff@pcc.edu>
2021-07-20 13:05:45 -0700

    variable needed to be long, not int

--- objectCode/stack64.c
+++ objectCode/stack64.c
@@ -7,13 +7,13 @@ long fn(long a, long b, long c, long d, long e, long f, long g)
 
 int main()
 {
-	int r =	fn(0xAAAAAAAAAAAAAAAA,
-			   0xBBBBBBBBBBBBBBBB,
-			   0xCCCCCCCCCCCCCCCC,
-			   0xDDDDDDDDDDDDDDDD,
-			   0xEEEEEEEEEEEEEEEE,
-			   0xFFFFFFFFFFFFFFFF,
-			   0x1111111111111111);
+	long r =	fn(0xAAAAAAAAAAAAAAAA,
+				   0xBBBBBBBBBBBBBBBB,
+				   0xCCCCCCCCCCCCCCCC,
+				   0xDDDDDDDDDDDDDDDD,
+				   0xEEEEEEEEEEEEEEEE,
+				   0xFFFFFFFFFFFFFFFF,
+			 	   0x1111111111111111);
 	
 	return 0;
 }

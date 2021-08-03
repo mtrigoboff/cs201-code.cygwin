1b5ac7834f3449617d83f7f713c092c542a46255
Michael Trigoboff <michael.trigoboff@pcc.edu>
2021-08-03 15:25:46 -0700

    print out # of iterations

--- optimize/measuringTime/burntime.c
+++ optimize/measuringTime/burntime.c
@@ -5,15 +5,16 @@
 #include <stdbool.h>
 #include <time.h>           // for clock()
 
-void burnTime(int seconds, bool debug)
+int burnTime(int seconds, bool debug)
 {
 	clock_t		startTime = clock();
 	clock_t		stopTime = startTime + seconds * CLOCKS_PER_SEC;
 	int			burn = 1;
+	int			iterations = 0;
 
 	if (stopTime < startTime) {
 		printf("clock wrapped around, try again\n");
-		return;
+		return -1;
 		}
 
 	if (debug)		// print debugging info
@@ -22,8 +23,12 @@ void burnTime(int seconds, bool debug)
 			   seconds, startTime, stopTime, CLOCKS_PER_SEC);
 
 	// burn up time
-	while(clock() < stopTime)
+	while(clock() < stopTime) {
 		burn *= 3; 
+		iterations++;
+		}
+	
+	return iterations;
 }
 
 #ifdef TEST
@@ -31,8 +36,10 @@ int main(int argc, char **argv)
 {
 	// if arg[1] present, use as number of seconds
 	int			seconds = argc > 1 ? atoi(argv[1]) : 3;       
+	int			iterations;
 
-	burnTime(seconds, argc > 2);
+	iterations = burnTime(seconds, argc > 2);
+	printf("iterations: %d\n", iterations);
 	return 0;
 }
 #endif

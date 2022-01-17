c4e924f2457999597105969d441cdcbb8fdea89a
Michael Trigoboff <michael.trigoboff@pcc.edu>
2022-01-17 15:32:15 -0800

    limited scope of vars, reduced output PRECISION, WIDTH

--- integers/pmsl.c
+++ integers/pmsl.c
@@ -1,16 +1,16 @@
 // Patriot missile example from B&O'H, Practice Problem 2.32.
 // For a given fraction, prints the following:
 //   the bit representation of the fractional binary number
-//   fpValue: the floating point value for the fraction
-//   fbValue: the actual value of the fractional binary representation
+//   calcValue: the floating point value for the fraction
+//   bitsValue: the actual value of the fractional binary representation
 //   error:   the difference between the "real" value and the fractional binary value
 //            (this prints in two formats, as a decimal number and in scientific notation)
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#define WIDTH       70
-#define PRECISION   60
+#define WIDTH       12
+#define PRECISION    8
 
 static char     helpStr[] = "call with 3 args: nBits numerator denominator";
 static double   oneHalf = 0.5;
@@ -20,10 +20,8 @@ int main (int argc, char **argv)
     int     nBits;
     int     numerator;
     int     denominator;
-    double  fpValue;        // floating point calculated value of fraction
     double  error;          // difference between fractional binary value and floating point value
     double  p2 = oneHalf;   // current power of 1/2 we're using
-    double  fbValue = 0;    // numeric value of the fractional binary representation
     int     b;
 
     if (argc != 4) {
@@ -40,8 +38,12 @@ int main (int argc, char **argv)
         return -2;
         }
 
-    fpValue = ((double) numerator) / (double) denominator;
-    error = fpValue;
+	{
+    double  calcValue;			// floating point calculated value of fraction
+    double  bitsValue = 0;		// numeric value of the fractional binary representation
+
+    calcValue = ((double) numerator) / (double) denominator;
+    error = calcValue;
     printf("\nnumerator = %d, denominator = %d\n", numerator, denominator);
     printf("\n%d bit value:    0.", nBits);
     
@@ -49,7 +51,7 @@ int main (int argc, char **argv)
     // simultaneously print out the fractional binary value
     for (b = 0; b < nBits; b++) {
         if (p2 <= error) {
-            fbValue += p2;      // continue computation of fractional binary value
+            bitsValue += p2;	// continue computation of fractional binary value
             error -= p2;        // continue computation of error
             printf("1");
             }
@@ -57,10 +59,11 @@ int main (int argc, char **argv)
             printf("0");
         p2 *= oneHalf;          // next power of 1/2
         }
-    printf("\n\nfpValue: %*.*f\n", WIDTH, PRECISION, fpValue);
-    printf(    "fbValue: %*.*f\n", WIDTH, PRECISION, fbValue);
-    printf(    "error:   %*.*f\n", WIDTH, PRECISION, error);
-    printf(    "         %*.*e\n", WIDTH, PRECISION - 4, error);
+    printf("\n\ncalcValue: %*.*f\n", WIDTH, PRECISION, calcValue);
+    printf(    "bitsValue: %*.*f\n", WIDTH, PRECISION, bitsValue);
+    printf(    "error:     %*.*f\n", WIDTH, PRECISION, error);
+    printf(    "           %*.*e\n", WIDTH, PRECISION - 4, error);
+	}
 
     printf("\n");
     return 0;
